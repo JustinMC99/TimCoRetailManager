98a2db55b87150830d72c49fce29e87b4efa6f37
justin cummings <justin.justinm@gmail.com>
2020-11-22 19:34:29 -0500

    added TRMDesktopUI and setup Container with Caliburn Micro for our MVVM framwork also includes the smiple container DI system

---
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRMDesktopUI.ViewModels;
+
+namespace TRMDesktopUI.ViewModels
+{
+    public class ShellViewModel
+    {
+    }
+}
